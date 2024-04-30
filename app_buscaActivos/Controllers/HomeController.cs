c85d29eb79d07a15b96307c0f9fb53e2fbc716f8
hsmg777 <138528665+hayland10@users.noreply.github.com>
2024-04-30 16:38:01 -0500

    commitPrueba

---
+++ app_buscaActivos/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using app_buscaActivos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace app_buscaActivos.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
