78d41f0eac2f6f61c4c12a3d826a10def559e0ef
hsmg777 <138528665+hayland10@users.noreply.github.com>
2024-05-01 12:50:41 -0500

    Front y diseño creado

---
+++ app_buscaActivos/Controllers/subdomainController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace app_buscaActivos.Controllers
+{
+    public class subdomainController : Controller
+    {
+        // GET: subdomainController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      
+    }
+}
