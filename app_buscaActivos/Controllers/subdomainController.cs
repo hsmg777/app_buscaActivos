8d74afc3bf3f65fefac8f2f5396251a583dd352c
hsmg777 <138528665+hayland10@users.noreply.github.com>
2024-05-10 20:21:59 -0500

    view partial view y consumo de api de SUBDOMINIO LISTA

--- app_buscaActivos/Controllers/subdomainController.cs
+++ app_buscaActivos/Controllers/subdomainController.cs
@@ -1,16 +1,50 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using app_buscaActivos.Models;
+using Newtonsoft.Json;
 
 namespace app_buscaActivos.Controllers
 {
-    public class subdomainController : Controller
+    public class SubdomainController : Controller
     {
-        // GET: subdomainController
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
+        private readonly HttpClient _httpClient;
 
-      
+        public SubdomainController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        // GET: SubdomainController
+       
+
+        [HttpPost]
+        public async Task<IActionResult> Search(string domain)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://subdomain-finder3.p.rapidapi.com/v1/subdomain-finder/?domain={domain}"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", "ca2de54915msh47c6f73f1c96837p100833jsnfdecc09caff8" },
+                    { "X-RapidAPI-Host", "subdomain-finder3.p.rapidapi.com" },
+                },
+            };
+
+            using (var response = await _httpClient.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var subdomains = JsonConvert.DeserializeObject<Subdominios>(responseBody);
+                return PartialView("_ResultsPartial", subdomains);
+            }
+        }
     }
 }
---
+++ app_buscaActivos/Models/Subdominios.cs
@@ -0,0 +1,15 @@
+﻿namespace app_buscaActivos.Models
+{
+    public class Subdominios
+    {
+        public string scan_date { get; set; }
+        public Subdomain[] subdomains { get; set; }
+    }
+
+    public class Subdomain
+    {
+        public string subdomain { get; set; }
+        public string ip { get; set; }
+        public bool cloudflare { get; set; }
+    }
+}
