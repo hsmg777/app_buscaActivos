01385524c51c837523b5116e866ad404d41f92a8
hsmg777 <138528665+hayland10@users.noreply.github.com>
2024-05-28 21:22:01 -0500

    falta ajustar cambios css de partial css

---
+++ app_buscaActivos/Controllers/socialNetsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using app_buscaActivos.Models;
+using Newtonsoft.Json;
+
+namespace app_buscaActivos.Controllers
+{
+    public class SocialNetsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SocialNetsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Search(string query)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://social-links-search.p.rapidapi.com/search-social-links?query={query}&social_networks=facebook%2Ctiktok%2Cinstagram%2Csnapchat%2Ctwitter%2Cyoutube%2Clinkedin%2Cgithub%2Cpinterest"),
+                Headers =
+                {
+                    { "x-rapidapi-key", "7b94842645msh93c56907e88affdp1ce6fcjsn7543715a97fb" },
+                    { "x-rapidapi-host", "social-links-search.p.rapidapi.com" },
+                },
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<socialNet.Rootobject>(body);
+                return PartialView("_SearchResults", data);
+            }
+        }
+    }
+}
---
+++ app_buscaActivos/Models/socialNet.cs
@@ -0,0 +1,25 @@
+﻿namespace app_buscaActivos.Models
+{
+    public class socialNet
+    {
+        public class Rootobject
+        {
+            public string status { get; set; }
+            public string request_id { get; set; }
+            public Data data { get; set; }
+        }
+
+        public class Data
+        {
+            public string[] facebook { get; set; }
+            public string[] instagram { get; set; }
+            public string[] twitter { get; set; }
+            public string[] linkedin { get; set; }
+            public string[] github { get; set; }
+            public string[] youtube { get; set; }
+            public string[] pinterest { get; set; }
+            public string[] tiktok { get; set; }
+            public string[] snapchat { get; set; }
+        }
+    }
+}
